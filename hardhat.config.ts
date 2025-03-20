48b4834d16a3fe34bf080c234c858ef64c0e011a
Spencer Miller <smiller@coti.io>
2025-03-20 13:59:26 +0200

    update hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -15,7 +15,7 @@ const config: HardhatUserConfig = {
         // do not include the metadata hash, since this is machine dependent
         // and we want all generated code to be deterministic
         // https://docs.soliditylang.org/en/v0.7.6/metadata.html
-        bytecodeHash: 'none',
+        bytecodeHash: "none",
       }
     }
   },
@@ -23,6 +23,11 @@ const config: HardhatUserConfig = {
     "coti-testnet": {
       url: "https://testnet.coti.io/rpc",
       chainId: 7082400,
+      accounts: [process.env.DEPLOYER_PRIVATE_KEY!]
+    },
+    "coti-mainnet": {
+      url: "https://mainnet.coti.io/rpc",
+      chainId: 2632500,
       accounts: [process.env.DEPLOYER_PRIVATE_KEY!]
     }
   },
@@ -41,11 +46,11 @@ const config: HardhatUserConfig = {
   },
   etherscan: {
     apiKey: {
-      testnet: 'empty'
+      "coti-testnet": 'empty'
     },
     customChains: [
       {
-        network: 'testnet',
+        network: "coti-testnet",
         chainId: 7082400,
         urls: {
           apiURL: "http://40.160.4.130/api",
